ebc45d43132c831791aafe9497ebb109bfd587b1
Ari Johannesson <arividar@gmail.com>
2020-06-17 17:29:54 +0000

    Minor refactoring

--- CapScr.Tests.ps1
+++ CapScr.Tests.ps1
@@ -15,8 +15,8 @@ Describe "CapScr Test Group" {
         }
         
         It 'should create current-datetime-named-screenshotfile if no filename supplied' {
-            $someDate = [DateTime]"07/06/2015 05:00:10" 
-            $dateFormatFileName = "yyyy-MM-ddTHH.mm.ss" 
+            [DateTime]$someDate = [DateTime]"07/06/2015 05:00:10" 
+            [String]$dateFormatFileName = "yyyy-MM-ddTHH.mm.ss" 
 
             mock -CommandName 'Get-Date' –MockWith {
                 param($Format)
