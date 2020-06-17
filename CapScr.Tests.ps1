030b17ab2da745ede2573c446683e6c8cbf231b7
Ari Johannesson <arividar@gmail.com>
2020-06-17 18:06:17 +0000

    Refactor test

--- CapScr.Tests.ps1
+++ CapScr.Tests.ps1
@@ -15,17 +15,15 @@ Describe "CapScr Test Group" {
         }
         
         It 'should create current-datetime-named-screenshotfile if no filename supplied' {
-            [DateTime]$someDate = [DateTime]"07/06/2015 05:00:10" 
-            [String]$dateFormatFileName = "yyyy-MM-ddTHH.mm.ss" 
-
+            Set-Variable someDate -Option Constant -Value ([DateTime]"07/06/2015 05:00:10")
+            Set-Variable dateFormatFileName -Option Constant -Value "yyyy-MM-ddTHH.mm.ss" 
+            Set-Variable screenShotFilename -Option Constant -Value "$($someDate.toString($dateFormatFileName)).jpg"
+            
             mock -CommandName 'Get-Date' –MockWith {
                 param($Format)
                 $Format | Should -Be $dateFormatFileName
                 $someDate.toString($Format)
             }
-
-            [String]$fileDateFormat = $someDate.toString($dateFormatFileName)
-            [String]$screenShotFilename = "$fileDateFormat.jpg"
 
             $screenShotFilename | Should -Not -Exist
             Write-Screenshot
