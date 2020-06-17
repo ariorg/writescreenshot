23ea4fb4775c1dae574f446cd93a41bf5809d1ab
Ari Johannesson <arividar@gmail.com>
2020-06-17 23:14:16 +0000

    Adds failing test for folder parameter

--- CapScr.Tests.ps1
+++ CapScr.Tests.ps1
@@ -3,22 +3,16 @@ Describe "CapScr Test Group" {
         . .\CapScr.ps1
     }
 
-    Context 'Write-ScreenshotWin tests' {
+    Context 'Write-Screenshot datetime-named file tests' {
         
         BeforeAll {
             Push-Location
             Set-Location $TestDrive
-        }
 
-        AfterAll { 
-            Pop-Location
-        }
-        
-        It 'should create current-datetime-named-screenshotfile if no filename supplied' {
             Set-Variable someDate -Option Constant -Value ([DateTime]"07/06/2015 05:00:10")
             Set-Variable dateFilenameFormat -Option Constant -Value "yyyy-MM-ddTHH.mm.ss" 
             Set-Variable screenShotFilename -Option Constant -Value "$($someDate.toString($dateFilenameFormat)).jpg"
-            
+
             mock -CommandName 'Get-Date' –MockWith {
                 param($Format)
 
@@ -26,9 +20,26 @@ Describe "CapScr Test Group" {
                 return $someDate.toString($Format)
             }
 
+        }
+
+        AfterAll {
+            Pop-Location
+        }
+
+        It 'should create datetime-named-screenshotfile in current dir if path not supplied' {
             $screenShotFilename | Should -Not -Exist
             Write-Screenshot
             $screenShotFilename | Should -Exist
+        }
+
+        It 'should create date-named screenshot file in the supplied folder' {
+            Set-Variable folder -Option Constant -Value (Join-Path $TestDrive '/TestLocation/File')
+            Write-Host $folder
+            Set-Variable fullPath -Option Constant -Value (Join-Path $folder $screenShotFilename)
+            Write-Host $fullPath
+            $fullPath | Should -Not -Exist
+            Write-Screenshot
+            $fullPath | Should -Exist
         }
     }
 }
