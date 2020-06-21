b688828262e906c19d0a373fdf79071cb1119ce7
Ari Johannesson <arividar@gmail.com>
2020-06-21 08:45:22 +0000

    Add example for Interval and Times parameters

--- Write-Screenshot.ps1
+++ Write-Screenshot.ps1
@@ -24,6 +24,12 @@ function Write-Screenshot() {
     Write-Screenshot -FolderPath /Screenshots -Filename screenshot10
 
     Creates a screenshot file /Screenshots/screenshot10.jpg
+    
+    .EXAMPLE
+    Write-Screenhot -FolderPath /Screenshots -Interval 600 -Times 10
+    
+    Creates screenshot every 10 minutes in /Screenshots as 2024-06-20_10.06.37.jpg, 
+    2024-06-20_20.06.37.jpg, 024-06-30_10.06.37.jpg etc.
     #>
  
     [CmdletBinding()]
